30e1cc48994f02ecd1af835c46462c1ea3f5c561
Roey_MR <roey.moses.rivlin@gmail.com>
2024-02-25 13:03:34 +0200

    add camera

--- tile.py
+++ tile.py
@@ -7,3 +7,4 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(r"C:\Users\roeym\PycharmProjects\Element_Rumble\tile assets\rock.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -10)
