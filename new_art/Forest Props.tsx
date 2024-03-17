fa98996586fc432ebc4d8325ac883946002e005b
Roey_MR <roey.moses.rivlin@gmail.com>
2024-03-17 13:28:20 +0200

    start example map with new art

---
+++ new_art/Forest Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Forest Props" tilewidth="16" tileheight="16" tilecount="1110" columns="30">
+ <image source="Forest Props.png" width="480" height="592"/>
+</tileset>
